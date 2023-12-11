519d27d1247c44c53fc6a3cb92e835f4037548ed
Julia Kostadinova <106996206+Julia-Kostadinova@users.noreply.github.com>
2023-12-12 00:41:19 +0200

    addional_check_correct input

--- scr/ATM_PROJECT/client_interface_messages.py
+++ scr/ATM_PROJECT/client_interface_messages.py
@@ -1,7 +1,17 @@
 
-def display_message(message):
-        display_message("Изберете опция:")
-        display_message("1. Вход")
-        display_message("2. Регистрация на нов акаунт")
+def display_main_menu():
+    display_message=print("Моля, изберете опция: \n 1. Вход  \n 2. Регистрация на нов акаунт")
+
+def get_user_input(prompt="Въведете Вашия избор: "):
+    while True:
+        try:
+            user_input = int(input(prompt))
+            if user_input in [1, 2]:
+                return user_input
+            else:
+                print("Грешка! Изборът трябва да бъде 1 или 2.")
+        except ValueError:
+            print("Грешка! Моля, въведете число.")
+
 
         
--- scr/main_code.py
+++ scr/main_code.py
@@ -1,7 +1,14 @@
 
 print( '***** PYTHON BANK *****')
 
-from src.ATM_PROJECT.client_interface_messages import display_message
+
+from ATM_PROJECT import client_interface_messages
+
+def main():
+    client_interface_messages.display_main_menu()
+    client_interface_messages.get_user_input()
 
 
 
+if __name__ == "__main__":
+    main()
